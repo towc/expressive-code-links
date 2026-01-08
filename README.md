32b28778aa792657775ca1265757788895e02331
Matei Copot <matei@copot.eu>
2026-01-08 16:06:07 +0100

    publish: 1.2.0

---
+++ README.md
@@ -0,0 +1,69 @@
+# Links
+
+A plugin for https://expressive-code.com/
+
+Allows you to add links to code blocks, by prepending them with `\\`:
+
+```js
+// comes from \\[towc/expressive-code-links](https://github.com/towc/expressive-code-links)
+import { pluginLink } from '\\[expressive-code-links]](https://github.com/towc/expressive-code-links)'
+
+// can have multiple links in one line!
+// usage as per \\[expressive code](https://expressive-code.com)'s \\[plugin documentation](https://expressive-code.com/reference/configuration/#plugins)
+
+// so for \\[astro](https://astro.build/):
+export default defineConfig({
+  integrations: [
+    expressiveCode({
+      plugins: [pluginLink()],
+    }),
+  ],
+})
+
+// doesn't mess with other common syntax!
+const array = [(x) => x]
+const one = array[0](1)
+
+const README = { md: 2 }
+const \\[link](README.md) = 0
+const two = array [link](README.md)
+```
+
+![example](./example.png)
+
+For a live example, see https://yourweekly.dev/learn-sql-from-js#js-refresher, the astro article I wrote this plugin for.
+
+## Customization
+
+If you e.g. wanted the underline to look differently, you can select the link element like this:
+
+```css
+.ec-link {
+  text-decoration: dotted underline;
+}
+```
+
+You might need to add `!important` if something else is targeting `a` elements with higher specificity in your site.
+
+The [mdn docs for text-decoration](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/text-decoration) are a good read for options.
+
+## Design choices
+
+You might already have markdown link syntax showcased in your codeblocks, and I didn't want the plugin to mess with that, which is why the actual `a` element is created by "escaping" the markdown syntax from the code block into "actual" markdown.
+
+The color of the link is inherited from what the underlying text should be. The `a.ec-link` element is otherwise completely vanilla, for accessibility and customizability.
+
+## Bugs
+
+- [ ] doesn't allow escaping of the link, e.g. `\\\\[link](href)` still renders as `\\link`
+  - not planning to implement, let me know if this affects you!
+- [ ] breaks when spanning multiple elements
+  - not planning to fix, let me know if you have a legitimate usecase!
+- [ ] doesn't auto-detect raw links like github
+  - not planning to implement. I would want to turn it off in my case.
+
+## Contributing
+
+I make no promises to keep this repository up to date.
+
+You're free to create issues and PRs, but your best bet is to copy the index.ts file in your own project, if you want to make modifications.
---
+++ src/index.ts
@@ -0,0 +1,110 @@
+import { definePlugin, ExpressiveCodeAnnotation, type AnnotationRenderOptions } from '@expressive-code/core'
+import { h } from '@expressive-code/core/hast';
+
+function getRegex() {
+  // ends up looking something like this: 
+  //
+  //   \ [link ] (href )
+  // /\\\[(.+)\]\((.+)\)/g
+
+  const capture = (r: string) => '(' + r + ')';
+  const escape = (c: string) => '\\' + c;
+  const allExcept = (c: string) => '[^' + c + ']';
+  const oneOrMore = (r: string) => r + '+';
+
+  const surrounded = (start: string, wrapper: (x: string) => string, end: string) =>
+    start + wrapper(oneOrMore(allExcept(end))) + end;
+
+  const link = surrounded(escape('['), capture, escape(']'))
+  const href = surrounded(escape('('), capture, escape(')'))
+
+  // currently doesn't allow escaping the escape
+  const regex = escape('\\') + link + href;
+
+  return new RegExp(regex, 'g');
+}
+
+export function pluginLink() {
+  return definePlugin({
+    name: 'Links inside code blocks, e.g. \\[link][href]',
+    hooks: {
+      preprocessCode: (context) => {
+        for (const line of context.codeBlock.getLines()) {
+          const matches = [...line.text.matchAll(getRegex())];
+
+          // account for multiple links in one line
+          let offset = 0;
+
+          for (const match of matches) {
+            const [original, link, href] = match;
+
+            const from = (match.index || 0) - offset;
+            const to = from + original.length;
+
+            line.addAnnotation(
+              new LinkAnnotation({
+                href,
+                inlineRange: {
+                  columnStart: from,
+                  columnEnd: to,
+                }
+              })
+            )
+            // this already culls annotation
+            line.editText(from, to, link);
+
+            offset += original.length - link.length;
+          }
+        }
+      }
+    },
+  })
+}
+
+class LinkAnnotation extends ExpressiveCodeAnnotation {
+  href: string;
+  constructor(options: ConstructorParameters<typeof ExpressiveCodeAnnotation>[0] & { href: string }) {
+    const { href, ...original } = options;
+    super(original);
+
+    this.href = href;
+  }
+  render({ nodesToTransform }: AnnotationRenderOptions) {
+    return nodesToTransform.map((node) => {
+      if (node.type !== 'element') {
+        // e.g.
+        //   con[st x](href) = 1
+        //   
+        // If EC considers `const` as a separate `span` element from `x`,
+        // which is needs to do to give them different colors,
+        // then this bit of code will break.
+        //
+        // I'm not aware of any other applicable scenarios.
+
+        type Node = object & { value?: string, children?: Node[] };
+
+        const getText = (el: Node): string =>
+          el.value ?? el.children?.map(getText).join('') ?? '';
+
+        throw new Error(
+          '[expressive-code-links]: Used link annotation spanning multiple expressive-code elements:\n'
+          + '  ' + getText(node)
+        );
+      }
+
+      return h(
+        node.tagName, 
+        node.properties,
+        h(
+          'a.ec-link',
+          {
+            href: this.href,
+            style: 'color: inherit',
+          },
+          ...node.children
+        )
+      )
+    })
+  }
+}
+
